50a37bcf54043de67a47061cd853629fbaf410b8
Ashraful Islam <theashrafislam@gmail.com>
2024-09-06 14:53:47 +0600

    all functionlity implemnt done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 // Middleware
@@ -84,6 +84,15 @@ async function run() {
                 res.status(500).send('Internal Server Error');
             }
         });
+
+
+        app.delete('/carts/:id',async (req, res) => {
+            const cartItemId = req.params.id;
+            const query = { _id: new ObjectId(cartItemId) };
+            const result = await cartCollections.deleteOne(query);
+            res.send(result);
+        });
+
 
 
 
